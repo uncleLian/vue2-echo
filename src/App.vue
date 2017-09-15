8314a89eac703ac96d469534033239f76a92b75b
uncleLian <771674109@qq.com>
2017-09-15 17:32:01 +0800

    取消打包css源码查找map文件

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@
 </template>
 <script>
 import '@/assets/css/reset.css'
-import 'material-design-icons/iconfont/material-icons.css'
 import fastClick from 'fastclick'
 import { mapGetters, mapMutations } from 'vuex'
 export default {
--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ Vue.config.productionTip = false
 import MuseUI from 'muse-ui'
 import 'muse-ui/dist/muse-ui.css'
 import 'muse-ui/dist/theme-teal.css'
+import 'material-design-icons/iconfont/material-icons.css'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
 import VueProgressBar from 'vue-progressbar'
